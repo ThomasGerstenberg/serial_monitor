e212961b3980fd75600b5ea90ed8d409e404ecda
Thomas Gerstenberg <thomas.gerstenberg@novoengineering.com>
2015-08-30 23:29:33 -0700

    updating mock to be a SerialBase for easier mocking

--- mock_serial/__init__.py
+++ mock_serial/__init__.py
@@ -1,18 +1,19 @@
 import time
+from serial.serialutil import SerialBase
 
-
-class Serial(object):
+class Serial(SerialBase):
     """
     Mock serial class
     """
     def __init__(self, comport, baud, timeout=1, **kwargs):
+        super(Serial, self).__init__(comport, baud, timeout=timeout, **kwargs)
         self.i = 0
-        self.port = comport
-        self.baud = baud
-        self.timeout = timeout
+
+    def _reconfigurePort(self):
+        pass
 
     def open(self):
-        print("Comport opened: {0}, baud:{1}".format(self.port, self.baud))
+        print("Comport opened: {0}, baud:{1}".format(self.port, self.baudrate))
 
     def close(self):
         print("Comport closed: {0}".format(self.port))
